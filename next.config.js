afa0fd822bdc3307a817aa033384984c1ccd28c3
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-13 16:28:32 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: 'experimental',
+  assetPrefix: 'experimental',
   trailingSlash: true,
   output: 'export',
 };
