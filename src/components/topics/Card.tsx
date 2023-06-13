be83f9bd9eb411fd5f110bae2440a8801ca236b0
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-13 15:04:41 +0900

    update

--- src/components/topics/Card.tsx
+++ src/components/topics/Card.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const Card = (props: Props) => {
   return (
     <Anchor className="group flex flex-col gap-2 rounded-3xl" href={props.path} unstyle={true}>
-      <div className="p-0 pb-4 transition-all duration-200 group-focus:p-4">
+      <div className="flex flex-col gap-4 p-0 pb-4 transition-all duration-200 group-focus:p-4">
         <img alt="" className="aspect-16/9 w-full rounded-3xl object-cover" src={props.imageUrl} />
         <h3 className="text-topics-card-title text-body">{props.desc}</h3>
       </div>
