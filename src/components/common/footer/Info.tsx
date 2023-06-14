f87260069a6e58ad6588a33457dcca81786be4a2
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-14 23:32:55 +0900

    update

--- src/components/common/footer/Info.tsx
+++ src/components/common/footer/Info.tsx
@@ -4,7 +4,7 @@ export const Info = () => {
   return (
     <div className="col-span-full flex flex-col items-center gap-2 text-center text-body md:col-span-6 md:col-start-7 md:items-start md:text-start">
       <Logo style={{ width: '131px', height: '24px' }} />
-      <p className="text-body-md">（法人番号：8000012010038）</p>
+      <p className="text-body-md">法人番号：8000012010038</p>
       <address className="text-body-md not-italic">
         〒102-0094 東京都千代田区紀尾井町1-3 東京ガーデンテラス紀尾井町 19階、20階
       </address>
