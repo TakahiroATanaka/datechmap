963615660c455fac793263da34cbcd9e0720bbdf
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-14 20:22:11 +0900

    update

--- src/components/topics/Card.tsx
+++ src/components/topics/Card.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export const Card = (props: Props) => {
   return (
-    <Anchor className="group flex flex-col gap-2 rounded-3xl" href={props.path} unstyle={true}>
-      <div className="flex flex-col gap-4 p-4">
+    <Anchor className="group flex flex-col gap-2 rounded-3xl !outline-offset-[16px]" href={props.path} unstyle={true}>
+      <div className="flex flex-col gap-4">
         <img alt="" className="aspect-16/9 w-full rounded-3xl object-cover" src={props.imageUrl} />
         <h3 className="text-topics-card-title text-body">{props.desc}</h3>
       </div>
