f1330b7574377f02c72c408fc85e16b1643e17ba
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-17 00:19:55 +0900

    update

--- src/components/common/header/Nav.tsx
+++ src/components/common/header/Nav.tsx
@@ -11,27 +11,27 @@ export const Nav = () => {
 
   return (
     <nav className="flex w-full items-center justify-end gap-6">
-      <ul className="hidden items-center gap-8 text-label-lg lg:flex">
+      <ul className="hidden items-center gap-3 text-label-lg lg:flex">
         <li>
           <UnstyledAnchor
-            className="rounded-full bg-wood-50 px-[10px] py-1 !font-extrabold hover:underline"
+            className="rounded-full bg-wood-50 px-4 py-1 !font-extrabold hover:underline"
             href="/experimental/"
           >
             ホーム
           </UnstyledAnchor>
         </li>
         <li>
-          <UnstyledAnchor className="px-[10px] hover:underline" href="/individual/">
+          <UnstyledAnchor className="px-4 hover:underline" href="/individual/">
             一般
           </UnstyledAnchor>
         </li>
         <li>
-          <UnstyledAnchor className="px-[10px] hover:underline" href="/business/">
+          <UnstyledAnchor className="px-4 hover:underline" href="/business/">
             行政・事業者
           </UnstyledAnchor>
         </li>
         <li className={`xl:after:ml-6 xl:after:content-["|"]`}>
-          <UnstyledAnchor className="px-[10px] hover:underline" href="/press/">
+          <UnstyledAnchor className="px-4 hover:underline" href="/press/">
             報道関係者
           </UnstyledAnchor>
         </li>
@@ -39,20 +39,20 @@ export const Nav = () => {
 
       <ul className="flex items-center gap-2 text-label-lg md:gap-3">
         <li>
-          <UnstyledAnchor className="flex items-center gap-2 px-[10px] hover:underline" href="/search/">
+          <UnstyledAnchor className="flex items-center gap-2 px-2 hover:underline md:px-4" href="/search/">
             <SearchIcon />
             検索
           </UnstyledAnchor>
         </li>
         <li className="hidden lg:list-item">
-          <UnstyledAnchor className="flex items-center gap-2 px-[10px] hover:underline" href="/en/">
+          <UnstyledAnchor className="flex items-center gap-2 px-4 hover:underline" href="/en/">
             Global Site
           </UnstyledAnchor>
         </li>
         <li>
           <Button
             aria-label={isOpen ? 'メニューを閉じる' : 'メニューを開く'}
-            className="flex items-center gap-2 px-[10px] text-body hover:underline"
+            className="flex items-center gap-2 px-2 text-body hover:underline md:px-4"
             id="btn-menu"
             onClick={() => setIsOpen(!isOpen)}
           >
