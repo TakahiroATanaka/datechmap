8d29c7ebad12f6bd463955e9fff6583bf24050d5
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-19 21:01:24 +0900

    update

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -3,11 +3,13 @@ import { Nav } from './Nav';
 
 export const Footer = () => {
   return (
-    <div className="w-full px-6">
-      <footer className="mx-auto grid w-full max-w-container grid-cols-1 gap-14 md:grid-cols-12 md:gap-8">
-        <Nav />
-        <Info />
-      </footer>
-    </div>
+    <footer className="w-full">
+      <div className="mx-auto max-w-container px-6">
+        <div className="grid grid-cols-1 gap-14 md:grid-cols-12 md:gap-8">
+          <Nav />
+          <Info />
+        </div>
+      </div>
+    </footer>
   );
 };
