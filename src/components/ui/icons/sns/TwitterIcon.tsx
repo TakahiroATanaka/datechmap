861f2d3b2ddc599841e0ed1e6577abe517f17708
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-21 20:35:25 +0900

    update

--- src/components/ui/icons/sns/TwitterIcon.tsx
+++ src/components/ui/icons/sns/TwitterIcon.tsx
@@ -1,7 +1,7 @@
 export const TwitterIcon = () => {
   return (
     <svg fill="none" height="32" viewBox="0 0 32 32" width="32">
-      <g clip-path="url(#clip0_4272_19032)">
+      <g clipPath="url(#clip0_4272_19032)">
         <path
           d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z"
           fill="#1B9DF0"
