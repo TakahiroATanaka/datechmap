2d5141ef28443351e008e64e04f963ae21dfeb7b
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-26 17:49:19 +0900

    update

--- src/components/procurement/Nav.tsx
+++ src/components/procurement/Nav.tsx
@@ -7,25 +7,25 @@ export const Nav = () => {
       <div className="grid grid-cols-2 gap-x-8 gap-y-10 lg:grid-cols-4">
         <UnstyledAnchor
           className="h-14 rounded-2xl border border-button-secondary-normal px-6 py-4 text-center text-button text-button-secondary-normal transition-all duration-200 hover:bg-button-secondary-hover"
-          href="/procurement/#"
+          href="/procurement/#tender_notice"
         >
           入札公告等
         </UnstyledAnchor>
         <UnstyledAnchor
           className="h-14 rounded-2xl border border-button-secondary-normal px-6 py-4 text-center text-button text-button-secondary-normal transition-all duration-200 hover:bg-button-secondary-hover"
-          href="/procurement/#"
+          href="/procurement/#planning_competition"
         >
           企画競争
         </UnstyledAnchor>
         <UnstyledAnchor
           className="h-14 rounded-2xl border border-button-secondary-normal px-6 py-4 text-center text-button text-button-secondary-normal transition-all duration-200 hover:bg-button-secondary-hover"
-          href="/procurement/#"
+          href="/procurement/#public_tender"
         >
           公募
         </UnstyledAnchor>
         <UnstyledAnchor
           className="h-14 rounded-2xl border border-button-secondary-normal px-6 py-4 text-center text-button text-button-secondary-normal transition-all duration-200 hover:bg-button-secondary-hover"
-          href="/procurement/#"
+          href="/procurement/#rfc_answer"
         >
           意見招請
         </UnstyledAnchor>
