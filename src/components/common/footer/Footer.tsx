0788d57af4920e47fdbb49e8f8c9b4272dcdb4dc
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-26 18:36:45 +0900

    update

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -7,8 +7,14 @@ export const Footer = () => {
     <footer className="w-full px-6">
       <div className="mx-auto max-w-container">
         <div className="grid grid-cols-1 gap-14 md:grid-cols-12 md:gap-8">
-          <div className="col-span-full mx-auto  md:col-span-3 md:mx-0">
-            <Logo style={{ width: '131px', height: '24px' }} />
+          <div className="col-span-full mx-auto flex flex-col justify-between gap-4 text-center md:col-span-3 md:mx-0 md:text-left">
+            <Logo className="mx-auto md:mx-0" style={{ width: '131px', height: '24px' }} />
+
+            <address className="text-pc-s not-italic text-description">
+              〒102-0094 東京都千代田区紀尾井町1-3 東京ガーデンテラス紀尾井町 20階
+            </address>
+
+            <p className="text-pc-s text-description">法人番号: 8000012010038</p>
           </div>
           <Nav />
           <Info />
--- src/components/policy/List.tsx
+++ src/components/policy/List.tsx
@@ -5,28 +5,16 @@ export const List = () => {
   return (
     <div className="grid grid-cols-1 content-stretch gap-x-8 gap-y-10 sm:grid-cols-2 lg:grid-cols-4">
       <Card
-        desc="マイナンバー制度・マイナンバーカードのご質問・ご不安にお答えします。"
+        desc="浅沼デジタル監より、2023年1月から3月までの四半期の活動内容を報告いたしました。"
         imageUrl={`${prefix()}/activities/myna.png`}
-        path="/policies/mynumber/"
-        title="マイナンバーカード"
+        path="/policies/periodic-briefing/"
+        title="デジタル庁の四半期活動報告を行いました"
       />
       <Card
-        desc="データに基づいたオープンな政策立案・実施を目指す取り組みの一環として実施しています。"
-        imageUrl={`${prefix()}/activities/dashboard.png`}
-        path="/resources/govdashboard/"
-        title="政策データダッシュボード"
-      />
-      <Card
-        desc="マイナンバーカードなしで、様々なサービスの利用や申し込みができるようになります。"
-        imageUrl={`${prefix()}/activities/sp-jpki.png`}
-        path="/policies/mynumber/smartphone-certification/"
-        title="スマホ用電子証明書搭載サービス"
-      />
-      <Card
-        desc="既存のサイト設計や表現を見直した、実証中のウェブサイトです。ぜひご体験ください。"
+        desc="マイナポータル実証アルファ版をリリースしました。スマートフォンにてぜひご体験ください。"
         imageUrl={`${prefix()}/activities/myna-alpha.png`}
         path="/policies/myna_portal/validation/"
-        title="マイナポータル実証アルファ版"
+        title="新しいマイナポータルを体験できます"
       />
     </div>
   );
