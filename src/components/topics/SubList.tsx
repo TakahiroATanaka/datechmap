7300a48dbd10fd69d7dcef5122c113c1f261e35d
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-28 13:31:59 +0900

    update

--- src/components/topics/SubList.tsx
+++ src/components/topics/SubList.tsx
@@ -5,7 +5,7 @@ export const SubList = () => {
   return (
     <div className="grid auto-rows-fr grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:grid-cols-3">
       <Card
-        imageUrl={`${prefix()}/topics/dummy.png`}
+        imageUrl={`${prefix()}/topics/kouzatenken.png`}
         path={'/news/a5e407c6-4505-44e8-86a9-447da533bb5f/'}
         title={'国民の皆様に安心していただけるよう公金受取口座の総点検を実施してまいります'}
       />
@@ -17,7 +17,7 @@ export const SubList = () => {
       />
 
       <Card
-        imageUrl={`${prefix()}/topics/dummy.png`}
+        imageUrl={`${prefix()}/topics/tousai.png`}
         path={'/policies/mynumber/smartphone-certification/'}
         title={'スマホ用電子証明書搭載サービスを開始します'}
       />
