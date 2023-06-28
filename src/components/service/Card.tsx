22344c741ee9861b4885b2d3bd5232462f54a800
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-28 21:01:40 +0900

    update

--- src/components/service/Card.tsx
+++ src/components/service/Card.tsx
@@ -12,7 +12,14 @@ type Props = {
 export const Card = (props: Props) => {
   return (
     <UnstyledAnchor className="flex flex-col gap-4 rounded-3xl !outline-offset-[12px]" href={props.url} target="_blank">
-      <img alt={props.alt} className="object-cover" height={256} loading="lazy" src={props.imageUrl} width={352} />
+      <img
+        alt={props.alt}
+        className="w-full object-cover"
+        height={256}
+        loading="lazy"
+        src={props.imageUrl}
+        width={352}
+      />
       <h3 className="text-pc-r">
         {props.title} <ExternalLinkIcon className="mb-1 ml-1 inline" />
       </h3>
