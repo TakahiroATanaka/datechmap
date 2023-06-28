1d17497366a7599138adbe2ad032248dc70c8852
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-06-28 13:54:36 +0900

    update

--- src/components/attention/AttentionSection.tsx
+++ src/components/attention/AttentionSection.tsx
@@ -7,7 +7,7 @@ export const AttentionSection = () => {
       <SectionHeader title="重要なお知らせ" />
 
       <Card
-        publishedAt={'2023年5月27日'}
+        publishedAt={'2023年05月27日'}
         title={
           <>
             マイナンバーカード関連サービスの誤登録等の事案について、ご自身での確認方法やご質問・ご不安にお答えします。マイナンバー総合フリーダイヤル
