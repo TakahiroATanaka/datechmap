935f918a70fa3e74f12f5c8e8a10557c3f25bf94
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-07-31 10:15:59 +0900

    update

--- src/components/procurement/Nav.tsx
+++ src/components/procurement/Nav.tsx
@@ -5,12 +5,28 @@ import { ExternalLinkIcon } from '../ui/icons/ExternalLinkIcon';
 export const Nav = () => {
   return (
     <div className="flex flex-col gap-12">
-      <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-4">
-        <LinkButton path="/procurement/#public_notice">入札公告等</LinkButton>
-        <LinkButton path="/procurement/#proposal">企画競争情報</LinkButton>
-        <LinkButton path="/procurement/#public_tender">公募情報</LinkButton>
-        <LinkButton path="/procurement/#rfc_answer">意見招請情報</LinkButton>
-      </div>
+      <ul className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-4">
+        <li>
+          <LinkButton className="inline-block w-full" path="/procurement/#public_notice">
+            入札公告等
+          </LinkButton>
+        </li>
+        <li>
+          <LinkButton className="inline-block w-full" path="/procurement/#proposal">
+            企画競争情報
+          </LinkButton>
+        </li>
+        <li>
+          <LinkButton className="inline-block w-full" path="/procurement/#public_tender">
+            公募情報
+          </LinkButton>
+        </li>
+        <li>
+          <LinkButton className="inline-block w-full" path="/procurement/#rfc_answer">
+            意見招請情報
+          </LinkButton>
+        </li>
+      </ul>
 
       <p className="text-pc-r">
         最新の情報は、
