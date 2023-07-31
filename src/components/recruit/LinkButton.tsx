3b9cba69b0919dbe5da3202c3f1e4639335c483b
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-07-31 10:12:16 +0900

    update

--- src/components/recruit/LinkButton.tsx
+++ src/components/recruit/LinkButton.tsx
@@ -4,12 +4,15 @@ import { ReactNode } from 'react';
 type Props = {
   children: ReactNode;
   path: string;
+  className?: string;
 };
 
 export const LinkButton = (props: Props) => {
   return (
     <UnstyledAnchor
-      className="rounded-2xl border border-divider px-6 py-4 text-pc-r leading-[22px] hover:bg-sumi-50 focus:border-transparent active:bg-sumi-50"
+      className={`rounded-2xl border border-divider px-6 py-4 text-pc-r leading-[22px] hover:bg-sumi-50 focus:border-transparent active:bg-sumi-50 ${
+        props.className ?? ''
+      }`}
       href={props.path}
     >
       {props.children}
--- src/components/recruit/content/Links.tsx
+++ src/components/recruit/content/Links.tsx
@@ -6,19 +6,45 @@ export const Links = () => {
     <div className="flex flex-col gap-8">
       <section className="flex flex-col gap-4">
         <h3 className="text-pc-m">業務説明会・イベント</h3>
-        <LinkButton path="/recruitment/recruiting-session/">開催情報</LinkButton>
+        <ul>
+          <li>
+            <LinkButton className="inline-block w-full" path="/recruitment/recruiting-session/">
+              開催情報
+            </LinkButton>
+          </li>
+        </ul>
       </section>
 
       <section className="flex flex-col gap-4">
         <h3 className="text-pc-m">新卒採用</h3>
-        <LinkButton path="/recruitment/newgraduates/">国家公務員採用試験</LinkButton>
+        <ul>
+          <li>
+            <LinkButton className="inline-block w-full" path="/recruitment/newgraduates/">
+              国家公務員採用試験
+            </LinkButton>
+          </li>
+        </ul>
       </section>
 
       <section className="flex flex-col gap-4">
         <h3 className="text-pc-m">中途採用</h3>
-        <LinkButton path="/recruitment/career/">中途採用</LinkButton>
-        <LinkButton path="/recruitment/career-government-official/">国家公務員採用試験</LinkButton>
-        <LinkButton path="/recruitment/career-selection-chief/">選考採用試験（一般職係長級）</LinkButton>
+        <ul className="flex flex-col gap-4">
+          <li>
+            <LinkButton className="inline-block w-full" path="/recruitment/career/">
+              中途採用
+            </LinkButton>
+          </li>
+          <li>
+            <LinkButton className="inline-block w-full" path="/recruitment/career-government-official/">
+              国家公務員採用試験
+            </LinkButton>
+          </li>
+          <li>
+            <LinkButton className="inline-block w-full" path="/recruitment/career-selection-chief/">
+              選考採用試験（一般職係長級）
+            </LinkButton>
+          </li>
+        </ul>
       </section>
 
       <CommonLinkButton className="md:hidden" path="/recruitment/">
