1005c9f17d9c3742a217b873a00c0949a086ff55
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-08-10 11:11:14 +0900

    update

--- src/components/ui/icons/CrossIcon.tsx
+++ src/components/ui/icons/CrossIcon.tsx
@@ -1,8 +1,8 @@
 export const CrossIcon = () => {
   return (
-    <svg fill="none" height="14" viewBox="0 0 14 14" width="14" xmlns="http://www.w3.org/2000/svg">
-      <rect fill="#1A1A1C" height="1" transform="rotate(45 1.27148 0.275391)" width="18" x="1.27148" y="0.275391" />
-      <rect fill="#1A1A1C" height="1" transform="rotate(-45 0.521484 13.0176)" width="18" x="0.521484" y="13.0176" />
+    <svg aria-label="" fill="none" height="25" viewBox="0 0 25 25" width="25" xmlns="http://www.w3.org/2000/svg">
+      <rect fill="#1A1A1C" height="1" transform="rotate(-45 6.0625 18.5103)" width="18" x="6.0625" y="18.5103" />
+      <rect fill="#1A1A1C" height="1" transform="rotate(45 6.76953 5.78223)" width="18" x="6.76953" y="5.78223" />
     </svg>
   );
 };
--- src/components/ui/icons/HamburgerMenuIcon.tsx
+++ src/components/ui/icons/HamburgerMenuIcon.tsx
@@ -1,9 +1,9 @@
 export const HamburgerMenuIcon = () => {
   return (
-    <svg fill="none" height="24" viewBox="0 0 24 24" width="24">
-      <rect fill="#1A1A1C" height="1" width="18" x="3" y="6.5" />
-      <rect fill="#1A1A1C" height="1" width="18" x="3" y="11.5" />
-      <rect fill="#1A1A1C" height="1" width="18" x="3" y="16.5" />
+    <svg aria-label="" fill="none" height="24" viewBox="0 0 25 24" width="25" xmlns="http://www.w3.org/2000/svg">
+      <rect fill="#1A1A1C" height="1" width="18" x="3.52002" y="6.5" />
+      <rect fill="#1A1A1C" height="1" width="18" x="3.52002" y="11.5" />
+      <rect fill="#1A1A1C" height="1" width="18" x="3.52002" y="16.5" />
     </svg>
   );
 };
