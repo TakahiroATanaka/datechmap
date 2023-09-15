7ef43825b7f629444967307a41ac2d546065ec85
takumi-hatta-dig <TakHatta@digital.go.jp>
2023-09-15 11:39:32 +0900

    u

--- src/components/common/footer/Info.tsx
+++ src/components/common/footer/Info.tsx
@@ -1,7 +1,7 @@
 import { UnstyledAnchor } from '@/components/ui/UnstyledAnchor';
 import { LinkedInIcon } from '@/components/ui/icons/sns/LinkedInIcon';
 import { NoteIcon } from '@/components/ui/icons/sns/NoteIcon';
-import { TwitterIcon } from '@/components/ui/icons/sns/TwitterIcon';
+import { XIcon } from '@/components/ui/icons/sns/XIcon';
 import { YouTubeIcon } from '@/components/ui/icons/sns/YouTubeIcon';
 
 export const Info = () => {
@@ -9,7 +9,7 @@ export const Info = () => {
     <div className="col-span-full flex flex-col justify-between gap-4 md:col-span-3">
       <div className="flex gap-4 self-center md:self-end">
         <UnstyledAnchor href="https://twitter.com/digital_jpn" target="_blank">
-          <TwitterIcon />
+          <XIcon />
         </UnstyledAnchor>
         <UnstyledAnchor href="https://www.youtube.com/channel/UCKmJk25wcPwCecf7nV9HwCw" target="_blank">
           <YouTubeIcon />
---
+++ src/components/ui/icons/sns/XIcon.tsx
@@ -0,0 +1,18 @@
+export const XIcon = () => {
+  return (
+    <svg aria-label="X" fill="none" height="32" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="16" cy="16" fill="black" r="16"></circle>
+      <g clipPath="url(#clip0_5331_796)">
+        <path
+          d="M17.5646 14.8417L22.9915 8.53333H21.7055L16.9934 14.0108L13.2298 8.53333H8.88889L14.5802 16.8162L8.88889 23.4315H10.175L15.1511 17.6471L19.1258 23.4315H23.4667L17.5643 14.8417H17.5646ZM15.8032 16.8892L15.2265 16.0644L10.6384 9.50147H12.6137L16.3164 14.7979L16.8931 15.6227L21.7062 22.5073H19.7308L15.8032 16.8895V16.8892Z"
+          fill="white"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_5331_796">
+          <rect fill="white" height="14.9058" transform="translate(8.88889 8.53333)" width="14.5778"></rect>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
